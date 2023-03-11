61246fb223f2d4bad19e9fcaed22dcc1d747b951
yidir <Yidirk@proton.me>
2023-03-11 20:49:48 +0100

    update facingMode

--- src/scan/scan.jsx
+++ src/scan/scan.jsx
@@ -79,9 +79,7 @@ const Scanner = () => {
         onError={handleError}
         onScan={handleScan}
         style={{ width: '100%', maxWidth: '400px', borderRadius: '5px', marginTop: '20px' }}
-        constraints={{
-          facingMode: 'environment'
-      }}
+        facingMode='rear'
       />
       {qrValue && (
         <div style={{ borderRadius: '10px', marginTop: '20px', maxWidth: '400px', width: '100%' }}>
